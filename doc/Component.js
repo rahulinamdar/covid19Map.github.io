e8b9eb202d9e3ee43c7767b258770c78f37a6f10
Inamadar Rahul <rahul.inamadar@evorait.com>
2020-04-13 10:56:39 +0000

    doc folder for github pages

---
+++ doc/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","com/rahul/covid19-map/model/models"],function(e,i,t){"use strict";return e.extend("com.rahul.covid19-map.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(t.createDeviceModel(),"device")}})});
---
+++ doc/test/unit/AllTests-dbg.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/rahul/covid19-map/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
